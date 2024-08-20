5caae7cb982cedb5f70bc449a1e0c5650e8da3fd
Pierluigi Viti <pierluigi@commercelayer.io>
2024-08-20 16:02:52 +0200

    feat: migrate to oclif/core v4 and oclif/test v4

--- test/commands/tags/types.test.ts
+++ test/commands/tags/types.test.ts
@@ -1,11 +1,10 @@
-import { expect, test } from '@oclif/test'
+import { expect } from 'chai'
+import { runCommand } from '@oclif/test'
+
 
 describe('tags:types', () => {
-  test
-    .timeout(15000)
-    .stdout()
-    .command(['tags:noc'])
-    .it('runs NoC', ctx => {
-      expect(ctx.stdout).to.contain('-= NoC =-')
-    })
+  it('runs NoC', async () => {
+    const { stdout } = await runCommand<{ name: string }>(['tags:noc'])
+    expect(stdout).to.contain('-= NoC =-')
+  }).timeout(15000)
 })
